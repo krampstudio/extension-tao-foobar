447e0d0167b5982c4ae812fefe956584a778570a
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2021-02-16 11:29:02 +0100

    feat: pretty print

--- views/js/controller/Foo/index.js
+++ views/js/controller/Foo/index.js
@@ -1,20 +1,20 @@
-define([], function(){
-    'use strict';
+define([], function() {
+  'use strict';
 
-    return {
-        start : function start(){
-            new Promise(function(resolve){
-                setTimeout(function(){
-                    resolve();
-                }, 150);
-            })
-            .then(function(){
-                console.log('Master of the time ?');
-                console.log('Master of puppets...');
-            })
-            .catch(function(err){
-                window.console.error('Master of the space ', err);
-            });
-        }
-    };
+  return {
+    start: function start() {
+      new Promise(function(resolve) {
+        setTimeout(function() {
+          resolve();
+        }, 150);
+      })
+        .then(function() {
+          console.log('Master of the time ?');
+          console.log('Master of puppets...');
+        })
+        .catch(function(err) {
+          window.console.error('Master of the space ', err);
+        });
+    },
+  };
 });
